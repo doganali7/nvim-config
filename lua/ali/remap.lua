e113dbd2d28d3f838bb597a4bf0d27802ce54119
doganali7 <alidogn98@gmail.com>
2026-05-21 23:23:08 +0300

    added white space indicator

--- lua/ali/remap.lua
+++ lua/ali/remap.lua
@@ -48,3 +48,8 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 
 -- Make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- Toggle whitespace visibility
+vim.keymap.set("n", "<leader>tw", function()
+    vim.opt.list = not vim.opt.list:get()
+end, { desc = "Toggle whitespace" })
--- lua/ali/set.lua
+++ lua/ali/set.lua
@@ -29,3 +29,13 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.opt.list = true
+vim.opt.listchars = {
+    space    = '·',
+    tab      = '→ ',
+    trail    = '•',
+    extends  = '»',
+    precedes = '«',
+    nbsp     = '␣',
+}
